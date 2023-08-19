04de6afb7eb68ea2e3eb872fd55fb9344027856c
mbimoad <bimoadjie19@gmail.com>
2023-08-19 13:37:52 +0700

    update'

--- app.js
+++ app.js
@@ -51,4 +51,4 @@ app.use('/experience', experienceroute)
 app.use('/login', authroute)
 app.use('/admin', adminroute)
 
-app.listen(port, () => console.log(`Dengarkan di Port ${port}`));
+app.listen(port, () => console.log(`Suara di Port ${port}`));
